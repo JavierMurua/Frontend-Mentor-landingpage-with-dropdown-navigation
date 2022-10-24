8a4ceefa0051d2c23518847f2350d7be65eb0c14
Javier Antonio Murúa <javierantoniomurua@gmail.com>
2022-10-24 18:36:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# landing-page-2
+ Frontend Mentor | Intro section with dropdown navigation
---
+++ js/menu.js
@@ -0,0 +1,5 @@
+const hamburguerIcon =document.querySelector('.nav__hamburguer');
+
+hamburguerIcon.addEventListener('click', ()=>{
+   hamburguerIcon.classList.toggle('nav__hamburguer--open'); 
+});
